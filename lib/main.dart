b545954316c0c36d6b24f34aad1fbedb589f9cc8
22shubh22 <22shubh22@gmail.com>
2020-09-12 12:11:30 +0530

    FindAPhone Changes
    https://github.com/tc2n/secohand/issues/1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:secohand/pages/findYourPhone.dart';
+import 'package:secohand/pages/findAPhone.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: FindYourPhone(),
+      home: FindAPhone(),
     );
   }
 }
