4654624bdff6bff56068fad15dd48dcad1a55003
tusharc2n <tusharc2n@gmail.com>
2020-09-12 15:17:12 +0530

    made cards smaller

--- lib/components/phoneCard.dart
+++ lib/components/phoneCard.dart
@@ -14,55 +14,58 @@ class PhoneCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 160,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 27.0, // soften the shadow
-            spreadRadius: 0.0, //extend the shadow
-            offset: Offset(
-              0.0, // Move to right 10  horizontally
-              1.0, // Move to bottom 10 Vertically
-            ),
-          )
-        ],
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Container(
-              height: 140,
-              width: 140,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                color: Colors.black26,
-                image: DecorationImage(
-                    image: AssetImage(url), fit: BoxFit.cover),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Container(
+        height: 150,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 27.0, // soften the shadow
+              spreadRadius: 0.0, //extend the shadow
+              offset: Offset(
+                0.0, // Move to right 10  horizontally
+                1.0, // Move to bottom 10 Vertically
+              ),
+            )
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Container(
+                height: 130,
+                width: 130,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.black26,
+                  image: DecorationImage(
+                      image: AssetImage(url), fit: BoxFit.cover),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(phoneBrand, style: brandName,),
-                Text(phoneModel, style: brandModel,),
-                SizedBox(height: 30,),
-                Text('$monthsOld months old', style: brandAge,),
-                Text('₹$price', style: brandName,),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(phoneBrand, style: brandName,),
+                  Text(phoneModel, style: brandModel,),
+                  SizedBox(height: 30,),
+                  Text('$monthsOld months old', style: brandAge,),
+                  Text('₹$price', style: brandName,),
+                ],
+              ),
             ),
-          ),
-          Expanded(child: Icon(FeatherIcons.arrowRight, color: primary, size: 35,)),
-        ],
+            Expanded(child: Icon(FeatherIcons.arrowRight, color: primary, size: 35,)),
+          ],
+        ),
       ),
     );
   }
